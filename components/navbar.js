bf76b6bd6542ff2ca8c529fed30798f0d886ec1b
RGarrettLee <reginaldgarrettlee@gmail.com>
2022-11-23 19:22:10 -0800

    fixed links

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar = function() {
     return (
     <div className="flex py-10 px-5">
         <ul className="list-none inline">
-          <a href='/'><li className="hover:text-blue-200 inline px-2">home</li></a>
-          <a href='/feedback'><li className="hover:text-blue-200 inline px-2">feedback</li></a>
-          <a href='#discord link'><li className="hover:text-blue-200 inline px-2">login</li></a>
+            <Link href='/'><li className="hover:text-blue-200 inline px-2">home</li></Link>
+            <Link href='/feedback'><li className="hover:text-blue-200 inline px-2">feedback</li></Link>
+            <Link href='#discord-login'><li className="hover:text-blue-200 inline px-2">login</li></Link>
         </ul>
       </div>
     )
