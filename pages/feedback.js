fe5aa0fd63c9e15c95aee05a9373d290b138d40f
RGarrettLee <reginaldgarrettlee@gmail.com>
2022-11-23 19:51:03 -0800

    added favicon and feedback info

--- pages/feedback.js
+++ pages/feedback.js
@@ -7,6 +7,10 @@ export default function Feedback() {
             <div className="flex justify-center align-center py-15 flex-col">
                 <h1 className="text-3xl font-bold text-center">Feedback</h1>
                 <h3 className="text-center">You will submit feedback here</h3>
+                <br/>
+                <br/>
+                <br/>
+                <h3 className="text-center font-bold">I will not read it</h3>
             </div>
         </div>
     )
