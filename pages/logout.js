162c63cda5428e0f3c6ae4d85d7200d34028106d
RGarrettLee <reginaldgarrettlee@gmail.com>
2022-11-26 04:11:32 -0800

    moved dependency into useEffect

--- pages/logout.js
+++ pages/logout.js
@@ -3,9 +3,9 @@ import { useRouter } from 'next/router';
 import supabase from "../db/connection";
 
 export default function Logout() {
-    const router = useRouter();
-
       useEffect(() => {
+        const router = useRouter();
+
         async function signout() {
           const { error } = await supabase.auth.signOut()
           router.reload(window.location.pathname);
