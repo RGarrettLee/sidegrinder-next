83145693483901bf998e674854a4e58a8adf34c3
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2023-02-08 04:41:11 -0500

    disabled blog till it works

--- components/navbar.js
+++ components/navbar.js
@@ -23,7 +23,7 @@ const Navbar = function() {
         <div className="flex justify-end py-10 px-5">
             <ul className="list-none inline">
                 <Link href='/'><li className="hover:text-blue-200 inline px-2">home</li></Link>
-                <Link href='#blog'><li className="hover:text-blue-200 inline px-2">blog</li></Link>
+                {/*<Link href='#blog'><li className="hover:text-blue-200 inline px-2">blog</li></Link>*/}
                 {/*<Link href='portfolio'><li className="hover:text-blue-200 inline px-2">portfolio</li></Link>*/}
                 <Link href='apps'><li className="hover:text-blue-200 inline px-2">apps</li></Link>
                 {!user.email ? (
