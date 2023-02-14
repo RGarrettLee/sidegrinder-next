683ec66394d6b0fcfbee6a0125803c2b2bd548f4
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2023-02-13 22:47:57 -0500

    fixed bug

--- pages/blog.js
+++ pages/blog.js
@@ -7,7 +7,7 @@ export default function Blog() {
    return (
       <>
          <h1 className='text-center text-2xl'>Blog WIP</h1>
-         <p className='mt-4 text-center font-thin text-lg'>create article page and user auth for seeing "create article"</p>
+         <p className='mt-4 text-center font-thin text-lg'>create article page and user auth for seeing *create article*</p>
       </>
    )
 }
