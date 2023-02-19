12ce3b33eb77b5f5a941dd6000a3226998bb0890
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2023-02-19 17:39:54 -0500

    fixed project tag gap

--- components/projectCard.js
+++ components/projectCard.js
@@ -14,12 +14,12 @@ export default function ProjectCard({ project }) {
                 </p>
             </div>
             <div className="px-6 pt-2 flex flex-row justify-center">
-                <a href={repo} target="_blank" rel="noreferrer"><span className="inline-block mr-2 bg-gray-700 hover:bg-gray-500 text-white font-semibold py-2 px-4 border border-gray-600 rounded ease-linear duration-150 transition-colors">GitHub</span></a>
-                <a href={deployment} target="_blank" rel="noreferrer"><span className="inline-block bg-gray-700 hover:bg-gray-500 text-white font-semibold py-2 px-4 border border-gray-600 rounded ease-linear duration-150 transition-colors">Deployed Link</span></a>
+                <a href={repo} target="_blank" rel="noreferrer"><span className="inline-block mr-2 bg-slate-800 hover:bg-gray-500 text-white font-semibold py-2 px-4 border border-gray-600 rounded ease-linear duration-150 transition-colors">GitHub</span></a>
+                <a href={deployment} target="_blank" rel="noreferrer"><span className="inline-block bg-slate-800 hover:bg-gray-500 text-white font-semibold py-2 px-4 border border-gray-600 rounded ease-linear duration-150 transition-colors">Deployed Link</span></a>
             </div>
-            <div className="px-6 pt-4 pb-2 flex flex-row justify-center">
+            <div className="px-6 pt-4 pb-2 flex flex-row justify-center gap-2">
                 {tags.map((tag, index) => (
-                    <span className="inline-block hover:cursor-default select-none bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" key={`tag ${index}`}>{`#${tag}`}</span>
+                    <span className="inline-block hover:cursor-default select-none bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2" key={`tag ${index}`}>{`#${tag}`}</span>
                 ))}
             </div>
         </div>
