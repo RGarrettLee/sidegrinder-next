75151c54455cd21f01338435c59bfe011043e324
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2023-11-18 04:17:53 -0500

    dubugging

--- pages/api/dailyDemon.js
+++ pages/api/dailyDemon.js
@@ -3,14 +3,13 @@ import supabase from '../../db/connection';
 export default function DailyDemon(req, res) {
    let daily = {name: 'daily'};
 
-   async function pushDemon(data) {
+   async function pushDemon(data, res) {
       const { error } = await supabase.from('dailies').update({data: data}).eq('type', 'demon');
+      res.status(200).json(error);
    }
 
    if (req.body) {
-      pushDemon(req.body);
-      daily = req.body;
-      res.status(200).json(req.body);
+      pushDemon(req.body, res);
    } else {
       res.status(200).json({response: daily});
    }
